f2a8c6ac7f42daefe78d163af51e644d77175a90
miz-ab <mizaby19@gmail.com>
2021-08-25 15:13:16 +0100

    create plot.py

---
+++ script/plot.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def plot_histogram(df:pd.DataFrame, column:str, color:str)->None:
+    plt.figure(figsize=(9, 7))
+    sns.displot(data=df, x=column, color=color, kde=True, height=7, aspect=2)
+    plt.title(f'Distribution of {column}', size=20, fontweight='bold')
+    plt.show()
