e41b8b7a7518189b731291a9ae8302b390718f40
miz-ab <mizaby19@gmail.com>
2021-08-25 08:19:56 +0100

    add load_file.py

---
+++ script/load_file.py
@@ -0,0 +1,8 @@
+import pandas as pd
+def read_csv(file_name):
+  try:
+    null_values = ["n/a", "na", "undefined"]
+    df = pd.read_csv(f'../data/{file_name}', na_values=null_values)
+    return df
+  except:
+    print("Log:-> File Not Found")
