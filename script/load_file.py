8f91fc476fca40bf0546acf31cc37cd7579f60a0
miz-ab <mizaby19@gmail.com>
2021-08-28 19:58:37 +0100

    updating load_file.py file

--- script/load_file.py
+++ script/load_file.py
@@ -4,6 +4,7 @@ import pandas as pd
 def read_csv(file_name):
   try:
     null_values = ["n/a", "na", "undefined"]
+    #load the csv from file 
     df = pd.read_csv(f'../data/{file_name}', na_values=null_values)
     return df
   except:
